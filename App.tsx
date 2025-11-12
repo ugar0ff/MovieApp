8ada8b7535314495311f5fb58d0efbde8ea3e1f4
Eugene Uvarov <eugene.uvarov@finstek.com>
2025-11-12 18:16:25 +0100

    Add persisting favorites movie

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import { store } from './src/store'
+import { store, persistor } from './src/store'
 import { extendTheme, NativeBaseProvider } from 'native-base'
 import { MainNavigation } from './src/navigation/MainNavigation'
 import { LogBox } from 'react-native'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Spinner } from 'native-base'
 
 LogBox.ignoreLogs([
   'In React 18, SSRProvider is not necessary and is a noop',
@@ -23,7 +25,9 @@ const App = () => {
   return (
     <Provider store={store}>
       <NativeBaseProvider theme={theme}>
-        <MainNavigation />
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+          <MainNavigation />
+        </PersistGate>
       </NativeBaseProvider>
     </Provider>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,17 +3,32 @@ import { tmdbApi } from './tmdbApi'
 import favoritesReducer from './favoritesSlice'
 import authReducer from './authSlice'
 import settingsReducer from './settingsSlice'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist'
+
+const favoritesPersistConfig = {
+  key: 'favorites',
+  storage: AsyncStorage,
+  whitelist: ['movies'],
+}
+
+const persistedFavoritesReducer = persistReducer(favoritesPersistConfig, favoritesReducer)
 
 export const store = configureStore({
   reducer: {
-    favorites: favoritesReducer,
+    favorites: persistedFavoritesReducer,
     auth: authReducer,
     settings: settingsReducer,
     [tmdbApi.reducerPath]: tmdbApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tmdbApi.middleware),
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(tmdbApi.middleware),
 })
 
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
